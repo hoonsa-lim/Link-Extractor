0ed70ef60f78dbee65d9c4dade8fb28de8285601
hslim <myohoon95@gmail.com>
2022-06-30 23:08:51 +0900

    코드 리펙토링

--- src/main/kotlin/LinkExtractor.kt
+++ src/main/kotlin/LinkExtractor.kt
@@ -21,6 +21,12 @@ class LinkExtractor {
          * regex
          */
         val WEB_URL_REGEX: Pattern = Pattern.compile("^(https?):\\/\\/([^:\\/\\s]+)(:([^\\/]*))?((\\/[^\\s/\\/]+)*)?\\/([^#\\s\\?]*)(\\?([^#\\s]*))?(#(\\w*))?$")
+
+        /**
+         * const text
+         */
+        const val DOMAIN_START_SIGN = "://"
+        const val PATH_DIVIDER = "/"
     }
     fun extractLegacy(url: String?): Link {
         if (url.isNullOrBlank()) throw  Exception(MSG_NULL_OR_BLANK)
@@ -28,7 +34,7 @@ class LinkExtractor {
         var domain: String? = null
         var subUrl: String? = null
 
-        val result = url.split("://")      //size 0인 경우도 있을 수 있음
+        val result = url.split(DOMAIN_START_SIGN)      //size 0인 경우도 있을 수 있음
 
         when(result.size){
             1 -> {
@@ -43,7 +49,7 @@ class LinkExtractor {
                 subUrl = domainSubUrl.second
             }
             else -> {
-                throw java.lang.Exception("url is only ://, or contains many ://")
+                throw java.lang.Exception("url is only $DOMAIN_START_SIGN, or contains many $DOMAIN_START_SIGN")
             }
         }
 
@@ -51,11 +57,11 @@ class LinkExtractor {
     }
 
     private fun extractDomainSubUrl(url: String): Pair<String?, String?>{
-        val result = url.replaceFirst("://", "").split("/")
+        val result = url.replaceFirst(DOMAIN_START_SIGN, "").split(PATH_DIVIDER)
 
         return try {
             Pair(result.firstOrNull(),
-                "/" + result.filterIndexed { i,_-> i > 0 }.joinToString("/"))
+                PATH_DIVIDER + result.filterIndexed { i,_-> i > 0 }.joinToString(PATH_DIVIDER))
         }catch (e: Exception){
             Pair(null, null)
         }
@@ -63,25 +69,26 @@ class LinkExtractor {
 
     fun extract(url: String?): Link {
         if (url.isNullOrBlank()) throw  IllegalArgumentException(MSG_NULL_OR_BLANK)
-        if (!url.contains("://")) throw  IllegalArgumentException("$MSG_REQUIRED_SCHEME\nurl == $url")
+        if (!url.contains(DOMAIN_START_SIGN)) throw  IllegalArgumentException("$MSG_REQUIRED_SCHEME\nurl == $url")
 
-        val result = url.split("://")
+        val result = url.split(DOMAIN_START_SIGN)
         if (result.size > 2) throw  IllegalArgumentException("$MSG_NOT_ALLOW_START_SIGN.\nurl == $url")
         if (result.size < 2) throw  IllegalArgumentException("$MSG_NOT_ALLOW_URL\nurl == $url")
         if (result.first() != "http" && result.first() != "https") throw  IllegalArgumentException("$MSG_NOT_ALLOW_SCHEME\nurl == $url")
 
-        val result2 = result[1].split("/", limit = 2)
+        val result2 = result[1].split(PATH_DIVIDER, limit = 2)
         if (result2.first().split(".").size < 2) throw  IllegalArgumentException("$MSG_NOT_ALLOW_DOMAIN.\nurl == $url")
 
-        return Link(result.first(), result2.first(), result2.lastOrNull()?.replaceFirstChar { "/$it" })
+        return Link(result.first(), result2.first(), result2.lastOrNull()?.replaceFirstChar { "$PATH_DIVIDER$it" })
     }
 
     fun extractRegex(url: String?): Link {
         if (url.isNullOrBlank()) throw  IllegalArgumentException(MSG_NULL_OR_BLANK)
         if (WEB_URL_REGEX.matcher(url).matches().not()) throw java.lang.IllegalArgumentException(MSG_NOT_ALLOW_URL)
 
-        val result = url.split("://")
-        val result2 = result[1].split("/", limit = 2)
-        return Link(result.first(), result2.first(), result2.lastOrNull()?.replaceFirstChar { "/$it" })
+        val schemeAndOther = url.split(DOMAIN_START_SIGN)
+        val domainAndSubUrl = schemeAndOther[1].split(PATH_DIVIDER, limit = 2)
+        val subUrl = domainAndSubUrl.lastOrNull()?.replaceFirstChar { "$PATH_DIVIDER$it" }
+        return Link(schemeAndOther.first(), domainAndSubUrl.first(), subUrl)
     }
 }
